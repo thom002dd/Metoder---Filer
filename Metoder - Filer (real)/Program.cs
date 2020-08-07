6e261fcae61c547a0230b16ae1039e9cb20a36f9
Thomas Egebjerg Scharbau Möller (thom002d) <thom002d@zbc.dk>
2020-08-07 09:06:42 +0200

    Add project files.

---
+++ Metoder - Filer (real)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metoder___Filer__real_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static void WriteAllText(string location, string contents);
+        {
+
+            string location = @".StarWars.txt";
+
+            File.WriteAllText(, "Han skød først")
+        }
+}
